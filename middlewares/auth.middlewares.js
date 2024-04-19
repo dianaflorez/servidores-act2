2b6481eeb5c4fc05adec85b2728b35eed7c3f98f
Diana <dianaflorezbravo@gmail.com>
2024-04-19 15:32:23 -0500

    login with session

--- middlewares/auth.middlewares.js
+++ middlewares/auth.middlewares.js
@@ -4,7 +4,7 @@ module.exports.sessions = sessions;
 
 module.exports.checkAuth = (req, res, next) => {
   const authHeader = req.header("Authorization");
-  const token = authHeader?.split('Bearer')[1];
+  const token = authHeader?.split('Bearer ')[1];
   console.log(token);
   console.log(sessions);
 
